7c8c2b5d67faeac0e16a818aab1fd34176369a36
StephanePeyregne <stephanepeyregne@gmail.com>
2025-11-26 23:02:36 +0000

    Deploying to gh-pages from @ StephanePeyregne/StephanePeyregne.github.io@4f0240ae3f24b8ca7b4e6e599bcb2907f1d0443e 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -3,8 +3,8 @@ const ninja = document.querySelector('ninja-keys');
 
 // add the home and posts menu items
 ninja.data = [{
-    id: "nav-about",
-    title: "about",
+    id: "nav-welcome-to-the-peyrégne-lab",
+    title: "Welcome to the Peyrégne Lab!",
     section: "Navigation",
     handler: () => {
       window.location.href = "/";
