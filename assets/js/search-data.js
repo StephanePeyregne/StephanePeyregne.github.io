f4073a5b463cba9310abea0b4f8d9d398e87a240
StephanePeyregne <stephanepeyregne@gmail.com>
2025-12-14 12:18:11 +0000

    Deploying to gh-pages from @ StephanePeyregne/StephanePeyregne.github.io@166eee702977083bca548e3e3003340acaf44ec8 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -75,10 +75,7 @@ ninja.data = [{
           description: "",
           section: "News",handler: () => {
               window.location.href = "/news/announcement_2/";
-            },},{id: "news-a-simple-inline-announcement-with-markdown-emoji-sparkles-smile",
-          title: 'A simple inline announcement with Markdown emoji! :sparkles: :smile:',
-          description: "",
-          section: "News",},{id: "news-the-peyrégne-lab-will-officially-start-on-february-1st-2026",
+            },},{id: "news-the-peyrégne-lab-will-officially-start-on-february-1st-2026",
           title: '✨ The Peyrégne Lab will officially start on February 1st 2026! ✨',
           description: "",
           section: "News",},{id: "projects-project-1",
