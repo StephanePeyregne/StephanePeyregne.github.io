9c6be4cb63a6d200a73ac0f01a5e5a1da106928a
StephanePeyregne <stephanepeyregne@gmail.com>
2025-12-14 11:41:03 +0000

    Deploying to gh-pages from @ StephanePeyregne/StephanePeyregne.github.io@eadc0b259751000f0f117246fa5fce0ef19cdeba 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -70,16 +70,16 @@ ninja.data = [{
           description: "",
           section: "Books",handler: () => {
               window.location.href = "/books/the_godfather/";
-            },},{id: "news-a-simple-inline-announcement",
-          title: 'A simple inline announcement.',
-          description: "",
-          section: "News",},{id: "news-a-long-announcement-with-details",
+            },},{id: "news-a-long-announcement-with-details",
           title: 'A long announcement with details',
           description: "",
           section: "News",handler: () => {
               window.location.href = "/news/announcement_2/";
             },},{id: "news-a-simple-inline-announcement-with-markdown-emoji-sparkles-smile",
           title: 'A simple inline announcement with Markdown emoji! :sparkles: :smile:',
+          description: "",
+          section: "News",},{id: "news-the-peyrégne-lab-will-officially-start-on-february-1st-2026",
+          title: '✨ The Peyrégne Lab will officially start on February 1st 2026! ✨',
           description: "",
           section: "News",},{id: "projects-project-1",
           title: 'project 1',
